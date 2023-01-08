ba1a437be63670379c73f112946c383321a76abc
nextbirthday <dumarsyoon@gmail.com>
2023-01-08 15:56:32 +0900

    tomato 브랜치 작업

---
+++ onionChat/src/main/java/server/TalkServer.java
@@ -0,0 +1,12 @@
+package server;
+
+import java.net.ServerSocket;
+
+public class TalkServer extends Thread {
+    
+    ServerSocket serverSocket = null;
+    public static void main( String[] args ) {
+        
+    }
+    
+}
