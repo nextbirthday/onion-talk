3429e15fe3d8461fe50d6df7d26f3f6dc90490d2
YeoEunGyeong <yeg0420@gmail.com>
2023-01-29 22:24:49 +0900

    chatview chatnotelogic 수정

--- onionChat/src/main/java/client/view/ChatView.java
+++ onionChat/src/main/java/client/view/ChatView.java
@@ -28,98 +28,97 @@ import model.ChatNoteLogic;
 
 public class ChatView extends JDialog implements ActionListener {
     // 선언
-    String    imagePath   = "src/main/resources/images/";
-    ImageIcon onionIcon   = new ImageIcon( imagePath + "onionicon.png" ); //
-    ImageIcon onionbgIcon = new ImageIcon( imagePath + "onionbg.png" ); //
-    JLabel    jlb_bgLabel = new JLabel( onionbgIcon ); //
+    String imagePath = "/src/main/resources/images/";
+    ImageIcon onionIcon = new ImageIcon(imagePath + "onionicon.png"); //
+    ImageIcon onionbgIcon = new ImageIcon(imagePath + "onionbg.png"); //
+    JLabel jlb_bgLabel = new JLabel(onionbgIcon); //
     // Container con = this.getContentPane();
-    JPanel         jp_centerPanel    = new JPanel(); // 텍스트 메인 페널
-    JPanel         jp_sendPanel      = new JPanel(); // 텍스트 전송 페널
-    JButton        jbtn_sendButton   = new JButton( "전송" ); // 텍스트 전송 버튼
-    JTextField     jtf_chatTextField = new JTextField( 24 ); // 텍스트 입력
-    StyledDocument sd_display        = new DefaultStyledDocument( new StyleContext() );
-    JTextPane      jtp_chatDisplay   = new JTextPane( sd_display );
-    JViewport      viewport          = new JViewport() {
-                                         public void paintComponent( Graphics g ) {
-                                             Image img = onionbgIcon.getImage(); //
-                                             setOpaque( false );
-                                             Graphics2D gd = ( Graphics2D ) g;
-                                             gd.setComposite( AlphaComposite.getInstance( AlphaComposite.SRC_OVER,
-                                                             0.7f ) );
-                                             g.drawImage( img, 0, 0, this );
-                                             super.paintComponent( g );
-                                         }
-                                     };
-    Font           font              = new Font( "굴림", Font.PLAIN, 13 );
-    JScrollPane    js_scrollPane     = new JScrollPane(); //
+    JPanel jp_centerPanel = new JPanel(); // 텍스트 메인 페널
+    JPanel jp_sendPanel = new JPanel(); // 텍스트 전송 페널
+    JButton jbtn_sendButton = new JButton("전송"); // 텍스트 전송 버튼
+    JTextField jtf_chatTextField = new JTextField(24); // 텍스트 입력
+    StyledDocument sd_display = new DefaultStyledDocument(new StyleContext());
+    JTextPane jtp_chatDisplay = new JTextPane(sd_display);
+    JViewport viewport = new JViewport() {
+        public void paintComponent(Graphics g) {
+            Image img = onionbgIcon.getImage(); //
+            setOpaque(false);
+            Graphics2D gd = (Graphics2D) g;
+            gd.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER,
+                    0.7f));
+            g.drawImage(img, 0, 0, this);
+            super.paintComponent(g);
+        }
+    };
+    Font font = new Font("굴림", Font.PLAIN, 13);
+    JScrollPane js_scrollPane = new JScrollPane(); //
     // 생성
-    
+
     public ChatView() {
         initDisplay();
     }
-    
+
     // 화면
     public void initDisplay() {
-        js_scrollPane.setOpaque( true );
-        js_scrollPane.setViewport( viewport );
-        jtp_chatDisplay.setOpaque( false );
-        js_scrollPane.setViewportView( jtp_chatDisplay );
+        js_scrollPane.setOpaque(true);
+        js_scrollPane.setViewport(viewport);
+        jtp_chatDisplay.setOpaque(false);
+        js_scrollPane.setViewportView(jtp_chatDisplay);
         // 메인 페널
-        jp_centerPanel.setLayout( new BorderLayout() );
-        jp_centerPanel.add( "Center", js_scrollPane );
-        jp_centerPanel.add( "South", jp_sendPanel );
+        jp_centerPanel.setLayout(new BorderLayout());
+        jp_centerPanel.add("Center", js_scrollPane);
+        jp_centerPanel.add("South", jp_sendPanel);
         // 전송 페널
-        jp_sendPanel.setLayout( new BorderLayout() );
-        jp_sendPanel.add( "Center", jtf_chatTextField );
-        jp_sendPanel.add( "East", jbtn_sendButton );
+        jp_sendPanel.setLayout(new BorderLayout());
+        jp_sendPanel.add("Center", jtf_chatTextField);
+        jp_sendPanel.add("East", jbtn_sendButton);
         // 버튼 크기
-        jbtn_sendButton.setPreferredSize( new Dimension( 65, 40 ) );
+        jbtn_sendButton.setPreferredSize(new Dimension(65, 40));
         // 전송 화면
         // jta_chatDisplay.setLineWrap(true); // 자동 줄바꿈
-        jtp_chatDisplay.setEditable( false ); // 텍스트 필드 입력 불가
+        jtp_chatDisplay.setEditable(false); // 텍스트 필드 입력 불가
         // 폰트
-        jtp_chatDisplay.setFont( font );
-        jtf_chatTextField.setFont( font );
+        jtp_chatDisplay.setFont(font);
+        jtf_chatTextField.setFont(font);
         // 액션
-        jtf_chatTextField.addActionListener( this );
-        jbtn_sendButton.addActionListener( this );
-        
-        setIconImage( onionIcon.getImage() ); // 아이콘 선언
-        setLayout( new GridLayout( 1, 2 ) );
-        add( jp_centerPanel );
-        setTitle( "양파쿵야TALK" );
-        setVisible( true );
-        setSize( 400, 600 );
+        jtf_chatTextField.addActionListener(this);
+        jbtn_sendButton.addActionListener(this);
+
+        setIconImage(onionIcon.getImage()); // 아이콘 선언
+        setLayout(new GridLayout(1, 2));
+        add(jp_centerPanel);
+        setTitle("양파쿵야TALK");
+        setVisible(true);
+        setSize(400, 600);
         // Dialog는 Frame에 종속되기 때문에 EXIT이 아니라 DISPOSE이다.
-        setDefaultCloseOperation( DISPOSE_ON_CLOSE ); // X 클릭 자동 종료
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE); // X 클릭 자동 종료
     }
-    
+
     // 메인
-    public static void main( String[] args ) {
+    public static void main(String[] args) {
         new ChatView();
     }
-    
+
     @Override
-    public void actionPerformed( ActionEvent e ) {
+    public void actionPerformed(ActionEvent e) {
         Object object = e.getSource();
-        
+
         // 전송 ??
-        if ( jbtn_sendButton == object || jtf_chatTextField == object ) {
+        if (jbtn_sendButton == object || jtf_chatTextField == object) {
             // 사용자가 입력한 메시지
             String message = jtf_chatTextField.getText(); // 전송 시 입력 메시지 받아옴
             // 사용자가 입력한 메시지를 받아서 소켓서버에 전송하기
             // ex) oos.writeObject(300|tomato|오늘뭐해?);
             // 서버소켓에 메시지를 전송하자마자 오라클 서버의 chatnode테이블에 insert문 처리하기
             ChatNoteLogic cn = new ChatNoteLogic();
-            
+
             try {
-                sd_display.insertString( sd_display.getLength(), message + "\n", null );
-            }
-            catch ( Exception e2 ) {
+                sd_display.insertString(sd_display.getLength(), message + "\n", null);
+            } catch (Exception e2) {
                 e2.printStackTrace();
             }
-            jtp_chatDisplay.setCaretPosition( sd_display.getLength() );
-            jtf_chatTextField.setText( "" ); // TextField 비움
+            jtp_chatDisplay.setCaretPosition(sd_display.getLength());
+            jtf_chatTextField.setText(""); // TextField 비움
         }
     }
 }
--- onionChat/src/main/java/model/ChatNoteLogic.java
+++ onionChat/src/main/java/model/ChatNoteLogic.java
@@ -11,78 +11,93 @@ import lombok.extern.log4j.Log4j2;
 import util.dto.ChatNote;
 import util.oracle.OracleConnection;
 
-@Log4j2( topic = "database" )
+@Log4j2(topic = "database")
 public class ChatNoteLogic {
-    private Connection        conn;
-    private PreparedStatement pstmt;
-    private ResultSet         rs;
-    
-    public ChatNoteLogic() {}
-    
-    public ChatNoteLogic( ChatView chatview ) {}
-    
-    /**
-     * @author eun
-     *         사용자와 1:1 혹은 1:n 채팅이 시작되면 입력한 메시지를 DB에 넣어준다
-     *         대화 종료 후 새롭게 대화 시작될 시 Chat_id, Chat_user를 확인하고 Chat_Note를 Chat_date를
-     *         시간순으로 가져온다
-     * @param
-     * @return
-     */
-    public List<ChatNote> chatNote() {
-        List<ChatNote> chatList = new ArrayList<>();
-        StringBuilder  sql      = new StringBuilder();
-        sql.append( "   SELECT ch_id, ch_user, ch_note, ch_date      " );
-        sql.append( "   FROM chatnote                 " );
-        
-        try {
-            conn = OracleConnection.getConnection();
-            pstmt = conn.prepareStatement( sql.toString() );
-            rs = pstmt.executeQuery();
-            ChatNote cn = null;
-            
-            while ( rs.next() ) {
-                cn = new ChatNote();
-                cn.setCh_id( rs.getString( "ch_id" ) );
-                cn.setCh_user( rs.getString( "ch_user" ) );
-                cn.setCh_note( rs.getString( "ch_note" ) );
-                cn.setCh_date( rs.getString( "ch_date" ) );
-                chatList.add( cn );
-            }
-            chatList.forEach( e -> log.debug( e ) );
-        }
-        catch ( Exception e ) {
-            log.error( "Exception :", e );
-        }
-        finally {
-            OracleConnection.freeConnection( conn, pstmt, rs );
-        }
-        return chatList;
-    }// end of chatNote
-    
-    // 소켓 접속해 채팅 시 소켓 서버에 전송되고 그 내용을 DB에 저장
-    public int chatMsgInsert( String user_id, String your_id, String ch_message ) {
-        int           result = 0;
-        StringBuilder sql    = new StringBuilder();
-        // sql.append("insert into chatnote values('user','tomato','메세지',sysdate)");
-        sql.append( "insert into chatnote values('?', '?', '?', sysdate)" );
-        
-        try {
-            conn = OracleConnection.getConnection();
-            pstmt = conn.prepareStatement( sql.toString() );
-            // 파라미터의 string 값에 ? 값으로 변경해서 사용
-            pstmt.setString( 1, user_id );
-            pstmt.setString( 2, your_id );
-            pstmt.setString( 3, ch_message );
-            result = pstmt.executeUpdate( sql.toString() );
-            log.debug( "result = {}", result );
-        }
-        catch ( Exception e ) {
-            log.error( "Exception :", e );
-        }
-        finally {
-            OracleConnection.freeConnection( conn, pstmt );
-        }
-        return result;
-    }
+   private Connection conn;
+   private PreparedStatement pstmt;
+   private ResultSet rs;
+
+   public ChatNoteLogic() {
+   }
+
+   public ChatNoteLogic(ChatView chatview) {
+   }
+
+   /**
+    * @author eun
+    *         사용자와 1:1 혹은 1:n 채팅이 시작되면 입력한 메시지를 DB에 넣어준다
+    *         대화 종료 후 새롭게 대화 시작될 시 Chat_id, Chat_user를 확인하고 Chat_Note를 Chat_date를
+    *         시간순으로 가져온다
+    * @param
+    * @return
+    */
+   public List<ChatNote> chatNote() {
+      List<ChatNote> chatList = new ArrayList<>();
+      StringBuilder sql = new StringBuilder();
+      sql.append("   SELECT ch_id, ch_user, ch_note, ch_date      ");
+      sql.append("   FROM chatnote                 ");
+
+      try {
+         conn = OracleConnection.getConnection();
+         pstmt = conn.prepareStatement(sql.toString());
+         rs = pstmt.executeQuery();
+         ChatNote cn = null;
+
+         while (rs.next()) {
+            cn = new ChatNote();
+            cn.setCh_id(rs.getString("ch_id"));
+            cn.setCh_user(rs.getString("ch_user"));
+            cn.setCh_note(rs.getString("ch_note"));
+            cn.setCh_date(rs.getString("ch_date"));
+            chatList.add(cn);
+         }
+         chatList.forEach(e -> log.debug(e));
+      } catch (Exception e) {
+         log.error("Exception :", e);
+      } finally {
+         OracleConnection.freeConnection(conn, pstmt, rs);
+      }
+      return chatList;
+   } // end of chatNote
+
+   // 소켓 접속해 채팅 시 소켓 서버에 전송되고 그 내용을 DB에 저장
+   public int chatMsgInsert(String user_id, String your_id, String ch_message) {
+      PreparedStatement pstmt = null;
+      int result = 0;
+      StringBuilder sql = new StringBuilder();
+      sql.append("insert into chatnote values(?, ?, ?, sysdate)");
+      try {
+         conn = OracleConnection.getConnection();
+         pstmt = conn.prepareStatement(sql.toString());
+         pstmt.setString(1, user_id);
+         pstmt.setString(2, your_id);
+         pstmt.setString(3, ch_message);
+         result = pstmt.executeUpdate();
+         System.out.println("result : " + result);
+      } catch (Exception e) {
+         e.printStackTrace();
+      } finally {
+         OracleConnection.freeConnection(conn, pstmt);
+      }
+      return result;
+   } // end of chatMsgInsert
+
+   /*
+    * // DB에 저장한 대화 내역 불러오기
+    * public List<String> chatMsgCall(String user_id, String your_id) {
+    * PreparedStatement pstmt = null;
+    * StringBuilder sql = new StringBuilder();
+    * sql.append("select ch_note, ch_date from chatnote order by ch_date");
+    * try {
+    * conn = OracleConnection.getConnection();
+    * pstmt = conn.prepareStatement(sql.toString());
+    * pstmt.setString(1, user_id);
+    * pstmt.setString(2, your_id);
+    * } catch (Exception e) {
+    * e.printStackTrace();
+    * } finally {
+    * OracleConnection.freeConnection(conn, pstmt);
+    * }
+    * }
+    */
 }
